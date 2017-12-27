6ab3b030c69678401875504324ee0e04bdea3317
Jhones Goncalves <jhones.goncalves@outlook.com>
2017-12-26 22:06:49 -0200

    calc

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,49 +1,65 @@
 var expect = require('chai').expect;
+var calc = require('../src/main.js');
 
-describe('Main', function () {
-  var arr;
-  // roda uma vez antes do bloco
-  before(function() {
-    // iniciar uma conexao com o banco
-    // cria um conunto de dados
+describe('Calc', function () {
+
+  // smoke tests
+  describe('smoke tests', function() {
+
+    it('should exist the calc lib', function() {
+      expect(calc).to.exist;
+    });
+
+    it('should exist the method `sum`', function() {
+      expect(calc.sum).to.exist;
+      expect(calc.sum).to.be.a('function');
+    });
+
+    it('should exist the method `sub`', function() {
+      expect(calc.sub).to.exist;
+      expect(calc.sub).to.be.a('function');
+    });
+
+    it('should exist the method `mult`', function() {
+      expect(calc.mult).to.exist;
+      expect(calc.mult).to.be.a('function');
+    });
+
+    it('should exist the method `div`', function() {
+      expect(calc.div).to.exist;
+      expect(calc.div).to.be.a('function');
+    });
   });
 
-  // roda uma vez depois do bloco
-  after(function() {
-    // fechar uma conexao com o banco
-    // apagar esse conjunto de dados
+  describe('Sum', function() {
+    it('sould return 4 when `sum(2,2)', function() {
+      expect(calc.sum(2,2)).to.be.equal(4);
+    });
   });
 
-  // roda todas as vezes antes de cada bloco
-  beforeEach(function() {
-    arr = [1,2,3];
+  describe('Sub', function() {
+    it('sould return 4 when `sub(6,2)`', function() {
+      expect(calc.sub(6,2)).to.be.equal(4);
+    });
+
+    it('sould return -4 when `sub(6,10)`', function() {
+      expect(calc.sub(6,10)).to.be.equal(-4);
+    });
   });
 
-  afterEach(function() {
+  describe('Div', function() {
+    it('sould return 2 when `div(4,2)`', function() {
+      expect(calc.div(4,2)).to.be.equal(2);
+    });
 
+     it('sould return `Nao e possivel divisao por zero!` when divibe by 0', function() {
+      expect(calc.div(4,0)).to.be.equal('Nao e possivel divisao por zero!');
+    });
   });
 
-  // testar tipo ou se existe (smoke test)
-  it('should be an array', function(){
-    expect(arr).to.be.an('array');
-  });
-
-  it('should have a size of 4 when push another value to the array', function() {
-    arr.push(4);
-    expect(arr).to.have.lengthOf(4);
-  });
-
-  it('should have a size of 3 when pop from the array', function() {
-    arr.pop();
-    expect(arr).to.not.include(3);
-  });
-
-  it('should return true when op 3 from the array', function(){
-    expect(arr.pop() === 3).to.be.true;
-  })
-
-  it('should remove 3 when use pop in the array', function() {
-     arr.pop();
-    expect(arr).to.have.lengthOf(2);
+  describe('Mult', function() {
+    it('sould return 4 when `mult(2,2)', function() {
+      expect(calc.mult(2,2)).to.be.equal(4);
+    });
   });
 });
